cf860bd4a1b6c212e044b08d62281c83ba584593
ppcrn <ppcrn-22ivyoxy@hotmail.com>
2016-02-24 10:19:32 +0700

    bindWidget เสร่จแล้วค้าบบบ

--- app/src/main/java/com/example/u/mydice/MainActivity.java
+++ app/src/main/java/com/example/u/mydice/MainActivity.java
@@ -2,12 +2,32 @@ package com.example.u.mydice;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageView ImgDice;
+    private Button btnBlack, btnRandom, btnNext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // bind widget
+        bindWidget();
+    }// main method
+
+    private void bindWidget() {
+
+        ImgDice = (ImageView) findViewById(R.id.imgDice);
+        btnBlack = (Button) findViewById(R.id.btnBlack);
+        btnRandom = (Button) findViewById(R.id.btnRandom);
+        btnNext = (Button) findViewById(R.id.btnNext);
+
+
     }
-}
+
+}// main class
